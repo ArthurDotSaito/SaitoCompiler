f72353d84aec86d7b297c65c64196b6a825f6225
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-27 18:41:16 -0300

    [feat] things working well

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "compiler.h"
 
 int main()
 {
+    int res = compile_file("./test.c", "./test", 0);
+    if (res == COMPILER_FILE_COMPILED_OK)
+    {
+        printf("File compiled successfully\n");
+    }
+    else if (res == COMPILER_FAILED_WITH_ERRORS)
+    {
+        printf("File compiled with errors\n");
+    }
+    else
+    {
+        printf("File compilation failed\n");
+    }
+
     return 0;
 }
