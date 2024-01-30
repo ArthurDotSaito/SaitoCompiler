63adefdd7c247c9a3ed0f1ea96e346f0c9444954
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-30 13:51:46 -0300

    [feat] implementing a compiler_error func

--- lexer.c
+++ lexer.c
@@ -7,9 +7,25 @@ static char peekc()
     return lex_process->function->peek_char(lex_process);
 }
 
+// Acess cprocess/compiler_process_push_char
+static char pushc(char c)
+{
+    lex_process->function->push_char(lex_process, c);
+}
+
 struct token *read_next_token()
 {
     struct token *token = NULL;
+    char c = peekc();
+    switch (c)
+    {
+    case EOF:
+        // Here, we have finished lexical analysis on the file
+        break;
+
+    default:
+        compiler_error(lex_process->compiler, "Unexpected token\n");
+    }
     return token;
 }
 
