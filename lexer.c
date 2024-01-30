560a1422a8ff5aae772ed61f7c6faf4a7b18884f
Arthur Saito <arthur_saito1998@hotmail.com>
2024-01-30 13:36:32 -0300

    [feat] basic lexer function

--- lexer.c
+++ lexer.c
@@ -1,11 +1,24 @@
 #include "compiler.h"
 static struct lex_process *lex_process;
 
+struct token *read_next_token()
+{
+    struct token *token = NULL;
+    return token;
+}
+
 int lex(struct lex_process *process)
 {
     process->current_expression_count = 0;
     process->parentheses_buffer = NULL;
     lex_process = process;
     process->pos.filename = process->compiler->cfile.abs_path;
-    return 0;
+
+    struct token *token = read_next_token();
+    while (token)
+    {
+        vector_push(process->token_vec, token);
+        token = read_next_token();
+    }
+    return LEXICAL_ANALYSIS_ALL_OK;
 }
