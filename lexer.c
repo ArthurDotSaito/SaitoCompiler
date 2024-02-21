8505f3d87d0a89879e242068f6354ecc30cc5986
Arthur Saito <arthur_saito1998@hotmail.com>
2024-02-20 22:37:33 -0300

    [feat] now it's possible to compile comments

--- lexer.c
+++ lexer.c
@@ -316,6 +316,64 @@ static struct token *token_make_operator_or_string()
     return token;
 }
 
+struct token *token_make_one_line_comment()
+{
+    struct buffer *buffer = buffer_create();
+    char c = nextc();
+    LEX_GETC_IF(buffer, c, c != '\n' && c != EOF);
+    return token_create(&(struct token){.type = TOKEN_TYPE_COMMENT, .sval = buffer_ptr(buffer)});
+}
+
+struct token *token_multiline_comment()
+{
+    struct buffer *buffer = buffer_create();
+    char c = nextc();
+    while (1)
+    {
+        LEX_GETC_IF(buffer, c, c != '*' && c != EOF);
+        if (c == EOF)
+        {
+            compiler_error(lex_process->compiler, "You did not close this multiline comment\n");
+        }
+        else if (c == '*')
+        {
+            nextc();
+
+            if (peekc() == '/')
+            {
+                nextc();
+                break;
+            }
+        }
+    }
+
+    return token_create(&(struct token){.type = TOKEN_TYPE_COMMENT, .sval = buffer_ptr(buffer)});
+}
+
+struct token *handle_comment()
+{
+    char c = peekc();
+    if (c == '/')
+    {
+        nextc();
+        if (peekc() == '/')
+        {
+            nextc();
+            return token_make_one_line_comment();
+        }
+        else if (peekc() == '*')
+        {
+            nextc();
+            return token_multiline_comment();
+        }
+
+        pushc('/');
+        return token_make_operator_or_string();
+    }
+
+    return NULL;
+}
+
 struct token *token_make_symbol()
 {
     char c = nextc();
@@ -364,6 +422,13 @@ struct token *read_next_token()
 {
     struct token *token = NULL;
     char c = peekc();
+
+    token = handle_comment();
+    if (token)
+    {
+        return token;
+    }
+
     switch (c)
     {
     NUMERIC_CASE:
---
+++ test.c
@@ -0,0 +1,1 @@
+// test comments. it should be alrigth
