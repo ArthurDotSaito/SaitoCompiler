75f8300df2624ed9bed8105e369d6580ba17031a
Arthur Saito <arthur_saito1998@hotmail.com>
2024-03-28 23:42:38 -0300

    add function to verify token_is_symbol or token_is_nl

--- parser.c
+++ parser.c
@@ -1,7 +1,30 @@
 #include "compiler.h"
 #include "helpers/vector.h"
 
-static struct compile_process *current_process;
+static struct compiler_process *current_process;
+
+static struct token *parser_last_token;
+
+static void parser_ignore_nl_or_comment(struct token *token)
+{
+    while (token && token_is_nl_or_comment_or_newline_separator(token))
+    {
+    }
+}
+
+static struct token *token_next()
+{
+    struct token *next_token = vector_peek_no_increment(current_process->token_vec);
+    parser_ignore_nl_or_comment(next_token);
+    current_process->pos = next_token->pos;
+    parser_last_token = next_token;
+    return vector_peek(current_process->token_vec);
+}
+
+int parse_next()
+{
+    return 0;
+}
 
 int parse(struct compiler_process *process)
 {
