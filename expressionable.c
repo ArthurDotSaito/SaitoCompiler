aa20a51452db0135c628dc17cf279b3294411722
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-03 22:33:50 -0300

    add first group of expressionable things

--- expressionable.c
+++ expressionable.c
@@ -1,3 +1,5 @@
+#include "compiler.h"
+
 #define TOTAL_OPERATOR_GROUPS 14
 #define MAX_OPERATORS_IN_GROUPS 12
 
@@ -7,8 +9,11 @@ enum
     ASSOCIATIBITY_RUGHT_TO_LEFT
 };
 
-struct expressionable_op_precedence_grour
+struct expressionable_op_precedence_group
 {
     char *operators[MAX_OPERATORS_IN_GROUPS];
     int associativity;
-}
+};
+
+struct expressionable_op_precedence_group op_precedence[TOTAL_OPERATOR_GROUPS] = {
+    {.operators = {"++", "--", "()", "[]", "(", "[", ".", "->", NULL}, .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT}};
