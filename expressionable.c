e33638b9d11cf9997ea79bcc7e00f78991d8065d
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-03 22:37:30 -0300

    add more groups of expressionable things

--- expressionable.c
+++ expressionable.c
@@ -6,7 +6,7 @@
 enum
 {
     ASSOCIATIVITY_LEFT_TO_RIGHT,
-    ASSOCIATIBITY_RUGHT_TO_LEFT
+    ASSOCIATIVITY_RIGHT_TO_LEFT
 };
 
 struct expressionable_op_precedence_group
@@ -16,4 +16,7 @@ struct expressionable_op_precedence_group
 };
 
 struct expressionable_op_precedence_group op_precedence[TOTAL_OPERATOR_GROUPS] = {
-    {.operators = {"++", "--", "()", "[]", "(", "[", ".", "->", NULL}, .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT}};
+    {.operators = {"++", "--", "()", "[]", "(", "[", ".", "->", NULL}, .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+    {.operators = {"*", "/", "%", NULL}, .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+    {.operators = {"<<", ">>", NULL}, .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+    {.operators = {"<", "<=", ">", ">=", NULL}, .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT}};
